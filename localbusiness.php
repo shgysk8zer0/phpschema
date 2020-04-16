711bdb85c838e48790f45955f2a3798cc81d878f
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-16 15:05:35 -0700

    Add price range for LocalBusiness

--- localbusiness.php
+++ localbusiness.php
@@ -1,18 +1,23 @@
 <?php
 namespace shgysk8zer0\PHPSchema;
 use \shgysk8zer0\PHPSchema\Interfaces\{PlaceInterface, GeoCoordinatesInterface};
-use \shgysk8zer0\PHPSchema\Traits\{PlaceTrait};
+use \shgysk8zer0\PHPSchema\Traits\{PlaceTrait, PriceRangeTrait};
 
 class LocalBusiness extends Organization implements PlaceInterface, GeoCoordinatesInterface
 {
 	use PlaceTrait;
+	use PriceRangeTrait;
+
 	public const TYPE = 'LocalBusiness';
 
 	public function jsonSerialize(): array
 	{
 		return array_merge(
 			parent::jsonSerialize(),
-			$this->_placeJSON()
+			$this->_placeJSON(),
+			[
+				'priceRange' => $this->getPriceRange(),
+			]
 		);
 	}
 }
