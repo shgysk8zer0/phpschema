95b02a990d1a3f054aa9f2938e5d6841fcd19024
Chris Zuber <shgysk8zer0@gmail.com>
2020-05-13 20:24:01 -0700

    Add langaue class & interface
    Also...
    - Add `homeLocation` & `workLocation` to `Person`
    - Add `alternateName` to `Thing`
    - Add missing `getName` & `setName` to `ThingInterface`

---
+++ language.php
@@ -0,0 +1,8 @@
+<?php
+namespace shgysk8zer0\PHPSchema;
+use \shgysk8zer0\PHPSchema\Interfaces\{LanguageInterface};
+
+class Language extends Intangible implements LanguageInterface
+{
+	public const TYPE = 'Language';
+}
